4af0e6a1a778ffb8e754c029278bd71508ba928c
Jose Bone <joserock7395@gmail.com>
2023-08-26 15:57:33 -0500

    Inicializacion proyecto

---
+++ src/main/java/ec/etsudesu/prueba/jose/bone/MsCompPruebaJoseBoneApplication.java
@@ -0,0 +1,13 @@
+package ec.etsudesu.prueba.jose.bone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompPruebaJoseBoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompPruebaJoseBoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/etsudesu/prueba/jose/bone/MsCompPruebaJoseBoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.etsudesu.prueba.jose.bone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompPruebaJoseBoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
